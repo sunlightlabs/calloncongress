a11250af5ab6e79a77e943b37b28573dacdce472
Dan Drinkard <dan.drinkard@gmail.com>
2012-04-24 15:38:06 -0400

    moar heroku fixing

--- runserver.py
+++ runserver.py
@@ -1,3 +1,3 @@
 import os
 from calloncongress import app, settings
-app.run(debug=settings.DEBUG, port=int(os.environ.get('PORT', 5000)))
+app.run(debug=settings.DEBUG, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
