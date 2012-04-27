2fde3b60a34eae6190ba36ef91dbe4f709bedbc9
Dan Drinkard <dan.drinkard@gmail.com>
2012-04-27 11:03:28 -0400

    add mongo uri skeleton vars

--- calloncongress/local_settings.example.py
+++ calloncongress/local_settings.example.py
@@ -1,4 +1,9 @@
 TWILIO_ACCOUNT_SID = ""
 TWILIO_AUTH_TOKEN = ""
 SUNLIGHT_KEY = ""
+# Safe to omit this if you're not using sentry
 SENTRY_DSN = ""
+# Include an appropriate MongoDB Connection URI unless you intend to connect to localhost/
+MONGOLAB_URI = ""
+MONGOHQ_URI = ""
+MONGO_URI = ""
