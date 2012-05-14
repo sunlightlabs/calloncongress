97782a8b83d74b6bf734b8aacb8a01afd674bd8a
Dan Drinkard <dan.drinkard@gmail.com>
2012-05-14 12:12:37 -0400

    s/party/state/

--- calloncongress/data.py
+++ calloncongress/data.py
@@ -227,7 +227,7 @@ def _format_bill(bill):
         bill_context.update(cosponsors="Cosponsored by: %s" %
             ', '.join(["%s, %s, %s" % (_format_legislator(cs)['fullname'],
                                        party_for(cs['party']),
-                                       state_for(cs['party'])) for cs in cosponsors]))
+                                       state_for(cs['state'])) for cs in cosponsors]))
 
     bill.update(bill_context=bill_context)
     return bill
