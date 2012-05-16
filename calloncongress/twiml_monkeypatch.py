1b06ffb376e53a6d454e4ba4f5f90044979615f1
Dan Drinkard <dan.drinkard@gmail.com>
2012-05-15 22:58:53 -0400

    requests status codes are integers

--- calloncongress/twiml_monkeypatch.py
+++ calloncongress/twiml_monkeypatch.py
@@ -1,6 +1,7 @@
 import os
 
 import twilio.twiml
+import requests
 from calloncongress.i18n import translate, translate_audio
 from calloncongress.helpers import get_lang
 from calloncongress import settings
@@ -13,10 +14,13 @@ class Say(twilio.twiml.Say):
             kwargs.update(language=lang)
 
         filename = translate_audio(text, **kwargs)
-        if os.path.isfile(filename):
-            return Play(filename, **kwargs)
-        else:
-            return super(Say, cls).__new__(cls, text, **kwargs)
+        if filename.startswith('/'):
+            if os.path.isfile(filename):
+                return Play(filename, **kwargs)
+        else:  # url
+            if requests.head(filename).status_code == 200:
+                return Play(filename, **kwargs)
+        return super(Say, cls).__new__(cls, text, **kwargs)
 
     def __init__(self, text, **kwargs):
         if 'language' not in kwargs.keys():
