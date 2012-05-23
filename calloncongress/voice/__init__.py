9358cbfc8683e244eac9264f88c3cacdcdf2a694
Dan Drinkard <dan.drinkard@gmail.com>
2012-05-23 16:01:57 -0400

    post to bio page when jumping back from bio page

--- calloncongress/voice/__init__.py
+++ calloncongress/voice/__init__.py
@@ -78,7 +78,7 @@ def member_bio():
     r = twiml.Response()
     bioguide = g.request_params['bioguide_id']
     legislator = read_context('legislator', load_member_for(bioguide))
-    with r.gather(numDigits=1, timeout=1, action=url_for('.member', bioguide_id=bioguide)) as rg:
+    with r.gather(numDigits=1, timeout=1, action=url_for('.member_bio', bioguide_id=bioguide)) as rg:
         rg.say(data.legislator_bio(legislator))
 
     return next_action(r, default=url_for('.member', bioguide_id=bioguide))
