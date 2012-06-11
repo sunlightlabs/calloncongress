ddedb3ec26c9900ab133333f5b766177790a6e81
Dan Drinkard <dan.drinkard@gmail.com>
2012-06-11 11:31:32 -0400

    add new relic to example settings

--- calloncongress/local_settings.example.py
+++ calloncongress/local_settings.example.py
@@ -7,6 +7,11 @@ GOOGLE_SERVICES_KEY = ""
 MONGOLAB_URI = ""
 MONGOHQ_URI = ""
 MONGO_URI = ""
+# Get a free New Relic account at http://newrelic.com
+NEW_RELIC_APP_NAME = ""
+NEW_RELIC_ID = ""
+NEW_RELIC_LICENSE_KEY = ""
+NEW_RELIC_LOG = "stdout"
 
 AUDIO_ROOT = "/static/audio"
 STATIC_VERSION = ""
